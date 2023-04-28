9cd78ee1fa647f2d6ae87901a14b92f75af5a7d3
EngineerPehr <dragontoothforge@gmail.com>
2023-04-28 08:12:56 -0600

    Polybius Encoder functioning

--- src/polybius.js
+++ src/polybius.js
@@ -4,15 +4,38 @@
 // of the anonymous function on line 6
 
 const polybiusModule = (function () {
-  // you can add any code you want within this function scope
+  const polybiusSquare = {'a': 11, 'b': 21, 'c': 31, 'd': 41, 'e': 51, 'f': 12, 'g': 22, 'h': 32, 'i': 42, 'j': 42, 'k': 52, 'l': 13, 'm': 23, 'n': 33, 'o': 43, 'p': 53, 'q': 14, 'r': 24, 's': 34, 't': 44, 'u': 54, 'v': 15, 'w': 25, 'x': 35, 'y': 45, 'z': 55}
+  const decoderSquare = {11: 'a', 21: 'b', 31: 'c', 41: 'd', 51: 'e', 12: 'f', 22: 'g', 32: 'h', 42: '(i/j)', 52: 'k', 13: 'l', 23: 'm', 33: 'n', 43: 'o', 53: 'p', 14: 'q', 24: 'r', 34: 's', 44: 't', 54: 'u', 15: 'v', 25: 'w', 35: 'x', 45: 'y', 55: 'z'}
 
-  function polybius(input, encode = true) {
-    // your solution code here
+  const encoder = (input = '') => {
+    // Message split into an array
+    const message = input.toLowerCase().split('')
+    // Loops through each element in the array and the polybius square
+      for (let i = 0; i < message.length; i++) {
+        const character = message[i]
+        for (key in polybiusSquare) {
+          const coordinate = polybiusSquare[key]
+          // When the array matches the polybius square, the array is updated with the new coordinate.
+          if (key === character) message[i] = coordinate
+        }
+      }
+      // Returns the array rejoined into a string
+      return message.join('')
+  }
+
+  function polybius(input = '', encode = true) {
+    if(encode === true) return encoder(input)
   }
 
   return {
     polybius,
-  };
-})();
+    encoder,
 
-module.exports = { polybius: polybiusModule.polybius };
+  }
+})()
+
+module.exports = { 
+  polybius: polybiusModule.polybius,
+  encoder: polybiusModule.encoder,
+
+ }
--- test/polybius.test.js
+++ test/polybius.test.js
@@ -1,1 +1,25 @@
-// Write your tests here!
+const expect = require('chai').expect
+const polybiusModule = require('../src/polybius')
+const polybius = polybiusModule.polybius
+const encoder = polybiusModule.encoder
+
+const unencodedInput = 'Three polygrams'
+const encodedOutput = '4432245151 534313452224112334'
+const encodedInput = '4432245151 534313452224112334'
+const decodedOutput = 'three polygrams'
+
+describe('polybius', () => {
+    it('should return an encoded message if encode is set to true', () => {
+        const actual = polybius(unencodedInput, true)
+        const expected = encodedOutput
+        expect(actual).to.equal(expected)
+    })
+})
+
+describe('encoder', () => {
+    it('should return an encoded message', () => {
+        const actual = encoder(unencodedInput)
+        const expected = encodedOutput
+        expect(actual).to.equal(expected)
+    })
+})
